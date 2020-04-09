5e527abd1b0d165731381fdeb6c7700957bb21fb
David Dueber <ddueber@gmail.com>
2020-04-09 08:18:34 -0400

    Convert example to use built-in dataset

--- R/BifactorIndices.R
+++ R/BifactorIndices.R
@@ -11,7 +11,7 @@
 #' @param UniLambda is a matrix of factor loadings or an object that can be converted to
 #' a matrix of factor loadings such as a fitted \pkg{lavaan} objects or fitted \pkg{mirt}
 #' object. Defaults to \code{NULL}, as \code{UniLambda} is only required if you wish to
-#' compute \code{\link{ARPB}}
+#' compute \code{\link{ARPB}}.
 #' @param standardized lets the function know whether to look for standardized or
 #' unstandardized results from \pkg{lavaan}. If \code{Lambda} is not a \pkg{lavaan} object,
 #' then \code{standardized} will be ignored.
@@ -60,16 +60,38 @@
 #' @examples
 #'
 #' # Computing bifactor indices from fitted lavaan object
-#' # (using mirt object is similar)
-#' HS_model_bifactor <- "visual  =~ x1 + x2 + x3
-#'                       textual =~ x4 + x5 + x6
-#'                       speed   =~ x7 + x8 + x9
-#'                       general =~ x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9"
-#' # lavaan cannot find a good solution, but that's ok since this is just for illustration
-#' fit <- suppressWarnings(lavaan::cfa(HS_model_bifactor,
-#'                    data = lavaan::HolzingerSwineford1939,
-#'                    orthogonal = TRUE))
-#' bifactorIndices(fit)
+#' # (using mirt object is similar). Use of the unidimensional
+#' # model is optional; it is only used to compute ARPB.
+#'
+#'SRS_UnidimensionalModel <-
+#'   "SRS =~ SRS_1  + SRS_2  + SRS_3  + SRS_4  + SRS_5  +
+#'           SRS_6  + SRS_7  + SRS_8  + SRS_9  + SRS_10 +
+#'           SRS_11 + SRS_12 + SRS_13 + SRS_14 + SRS_15 +
+#'           SRS_16 + SRS_17 + SRS_18 + SRS_19 + SRS_20"
+#'
+#'SRS_Unidimensional <- lavaan::cfa(SRS_UnidimensionalModel,
+#'                                  SRS_data,
+#'                                  ordered = paste0("SRS_", 1:20),
+#'                                  orthogonal = TRUE)
+#'
+#'
+#' SRS_BifactorModel <-
+#' "SRS =~ SRS_1  + SRS_2  + SRS_3  + SRS_4  + SRS_5  +
+#'         SRS_6  + SRS_7  + SRS_8  + SRS_9  + SRS_10 +
+#'         SRS_11 + SRS_12 + SRS_13 + SRS_14 + SRS_15 +
+#'         SRS_16 + SRS_17 + SRS_18 + SRS_19 + SRS_20
+#'  Function     =~ SRS_5  + SRS_9  + SRS_12 + SRS_15 + SRS_18
+#'  Pain         =~ SRS_1  + SRS_2  + SRS_8  + SRS_11 + SRS_17
+#'  SelfImage    =~ SRS_4  + SRS_6  + SRS_10 + SRS_14 + SRS_19
+#'  MentalHealth =~ SRS_3  + SRS_7  + SRS_13 + SRS_16 + SRS_20"
+#'
+#' SRS_bifactor <- lavaan::cfa(SRS_BifactorModel,
+#'                             SRS_data,
+#'                             ordered = paste0("SRS_", 1:20),
+#'                             orthogonal = TRUE)
+#'
+#' bifactorIndices(SRS_bifactor, UniLambda = SRS_Unidimensional)
+#'
 #'
 #'
 #' # Computing bifactor indices from standardized factor loading matrices
@@ -87,10 +109,9 @@
 #'                     .55,   0,   0, .75),
 #'                     ncol = 4, byrow = TRUE)
 #' colnames(Lambda) <- c("General", "SF1", "SF2", "SF3")
-#' UniLambda <- c(.78, .84, .82, .77, .69, .62, .69, .66, .82, .56, .74, .65)
-#' bifactorIndices(Lambda, UniLambda = UniLambda)
+#' bifactorIndices(Lambda)
 #'
-bifactorIndices <- function(Lambda, Theta = NULL, UniLambda = NULL, standardized = TRUE, Psi = NULL) {
+bifactorIndices <- function(Lambda, Theta = NULL, UniLambda = NULL, standardized = TRUE, Phi = NULL) {
   ## if fitted mirt object, then throw warning about Omegas probably being meaningless
   if ("SingleGroupClass" %in% class(Lambda)) {
     warning("Interpreting omega indices for IRT models is not recommended at this time")
@@ -101,11 +122,11 @@ bifactorIndices <- function(Lambda, Theta = NULL, UniLambda = NULL, standardized
   if (is.null(Theta)) {Theta = getTheta(Lambda, standardized = standardized)}
 
   # Can do Phi for SingleGroupClass and for lavaan
-  if (is.null(Psi)) {
+  if (is.null(Phi)) {
     if ("SingleGroupClass" %in% class(Lambda)) {
-      Psi <- mirt::summary(Lambda)$fcor
+      Phi <- mirt::summary(Lambda)$fcor
     } else if ("lavaan" %in% class(Lambda)) {
-      Psi <- lavaan::lavInspect(Lambda, "std")$psi
+      Phi <- lavaan::lavInspect(Lambda, "std")$psi
       # I hate that dumb symmetric matrix print method
       class(Psi) <- "matrix"
     }
@@ -114,8 +135,8 @@ bifactorIndices <- function(Lambda, Theta = NULL, UniLambda = NULL, standardized
 
   Lambda <- getLambda(Lambda, standardized = standardized)
 
-  # If Psi is still NULL, let's make it the identity matrix
-  if (is.null(Psi)) {Psi <- diag(nrow = nrow(Lambda))}
+  # If Phi is still NULL, let's make it the identity matrix
+  if (is.null(Phi)) {Phi <- diag(nrow = nrow(Lambda))}
 
   if (!is.null(UniLambda)) {UniLambda <- getLambda(UniLambda, standardized = standardized)}
 
@@ -127,8 +148,8 @@ bifactorIndices <- function(Lambda, Theta = NULL, UniLambda = NULL, standardized
                             OmegaH  = Omega_H(Lambda, Theta))
   if (standardized) {
     FactorLevelIndices[["H"]] <- H(Lambda)
-    if (!is.null(Psi)) {
-      FactorLevelIndices[["FD"]] <- FD(Lambda)
+    if (!is.null(Phi)) {
+      FactorLevelIndices[["FD"]] <- FD(Lambda, Phi)
     }
   }
 
@@ -146,6 +167,7 @@ bifactorIndices <- function(Lambda, Theta = NULL, UniLambda = NULL, standardized
   ItemLevelIndices <- ItemLevelIndices[which(!sapply(ItemLevelIndices, is.null))]
   ItemLevelIndices <- as.data.frame(ItemLevelIndices)
   if (isTRUE(all.equal(dim(ItemLevelIndices), c(0, 0)))) {ItemLevelIndices <- NULL}
+  if (ncol(ItemLevelIndices) == 2) {colnames(ItemLevelIndices)[2] <- "RelParBias"}
 
   ## Model level indices next
   if (is.null(getGen(Lambda))) { # No general factor
