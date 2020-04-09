15a109fcdbf425eed5758e9a291289756506addf
David Dueber <ddueber@gmail.com>
2020-04-08 23:10:22 -0400

    Add documentation for the datasets.

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Response Data to the SRS-22r
+#'
+#'A dataset containing 500 responses to the 20 item SRS-22r scoliosis
+#'quality of life outcome measure.
+#'
+#'The Function subscale consists of items 5, 9, 12, 15, and 18.
+#'The Pain subscale consists of items 1, 2, 8, 11, and 17.
+#'The SelfImage subscale consists of items 4, 6, 10, 14, and 19.
+#'The MentalHealth subscale consists of items 3, 7, 13, 16, and 20.
+#'
+#'@format A data frame with 500 rows and 20 columns
+#'Rownames indicate the item number
+#'
+
+"SRS_data"
+
+
+#' Simulated data for multi-trait multi-method
+#'
+#'A dataset containing 500 responses to a hypothetical 27 item instrument
+#'measuring three traits from three different sources. Each trait is measured
+#'by 3 items collected from each of three sources (methods).
+#'
+#'@format A data frame with 500 rows and 27 columns
+#' The item names indicate the trait number, method number, and item number
+
+"MTMM_data"
